cc05f4fa914da96e0ea5e77e684b4b9291664995
MacMil <milan@wintruff.de>
2025-06-19 15:32:15 +0200

    updated time logic

--- commands/sharefile.js
+++ commands/sharefile.js
@@ -1,21 +1,30 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const formatDate = require('../utils/formatDate');
+const parseTimestamp = require('../utils/parseTimestamp');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('sharefile')
     .setDescription('Share a file with metadata')
     .addStringOption(option =>
-        option.setName('link').setDescription('The Link to your File').setRequired(true))
+      option.setName('link')
+        .setDescription('The Link to your File')
+        .setRequired(true))
     .addStringOption(option =>
-      option.setName('title').setDescription('The title of the file').setRequired(true))
+      option.setName('title')
+        .setDescription('The title of the file')
+        .setRequired(true))
     .addStringOption(option =>
-      option.setName('details').setDescription('Optional details').setRequired(false))
+      option.setName('details')
+        .setDescription('Optional details')
+        .setRequired(false))
     .addStringOption(option =>
-      option.setName('lastupdate').setDescription('Last update date/time').setRequired(false))
+      option.setName('lastupdate')
+        .setDescription('Last update date/time')
+        .setRequired(false))
     .addUserOption(option =>
-      option.setName('mention').setDescription('Tag a user')),
-    
+      option.setName('mention')
+        .setDescription('Tag a user')),
+
   async execute(interaction) {
     const title = interaction.options.getString('title');
     const link = interaction.options.getString('link');
@@ -26,7 +35,7 @@ module.exports = {
     const embed = new EmbedBuilder()
       .setTitle(title)
       .setURL(link)
-      .setColor(interaction.member.displayColor || 0x2f3136) // fallback to Discord embed bg gray if something goes wrong
+      .setColor(interaction.member.displayColor || 0x2f3136)
       .addFields({ name: 'Link', value: `[Open File](${link})`, inline: false })
       .setFooter({ text: `Filed by ${interaction.user.username}` })
       .setTimestamp();
@@ -36,8 +45,12 @@ module.exports = {
     }
 
     if (lastUpdateInput) {
-      const formatted = formatDate(lastUpdateInput);
-      embed.addFields({ name: 'Last Update', value: formatted || lastUpdateInput, inline: true });
+      const formatted = parseTimestamp(lastUpdateInput);
+      embed.addFields({
+        name: 'Last Update',
+        value: formatted || 'Invalid date format',
+        inline: true
+      });
     }
 
     const content = mention ? `${mention}` : null;
